c522d62d4e00179b4d5e57e8b7131fa7b7d24b90
Twinkal Pansuriya <twinklepansuriya4124@gmail.com>
2024-12-24 19:05:56 +0530

    Read me

---
+++ variables.php
@@ -0,0 +1,49 @@
+<!-- Varible is define the $  -->
+
+<!-- The variable are storage location or 
+     The variables are an abstract storage location.
+     The variables can store any kind of data.
+     Example. Name,Number etc.... 
+     
+     
+     👉Use of variables:-
+     👉The same data is used in multiple times.
+     👉display the name in multiple places on the website page.
+     👉Usr the same number multiple time.      
+-->
+
+<?php
+
+    $name = "Twinkal Pansuriya";
+    echo $name;
+    echo "<br>";
+
+    echo $name;
+    echo "<br>";
+
+    echo $name;
+    echo "<br>";
+
+    echo $name;
+    echo "<br>";
+
+    echo $name;
+    echo "<br>";
+
+    
+    $num = 10;
+
+    echo $num * 80 ;
+    echo "<br>";
+
+    echo $num * 60;
+    echo "<br>";
+
+    echo $num * 30;
+    echo "<br>";
+
+    echo $num * 20;
+    echo "<br>";
+
+
+?>
